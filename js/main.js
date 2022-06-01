2c88cec9a571074497c369948638ceacb2997f53
Lazar-J <103836075+Lazar-J@users.noreply.github.com>
2022-06-01 18:28:05 +0200

    Make grid different size
    So I made a button for user, so he can enter some number between 16 and 100, and based on that grid will be creadted NxN.
    I also added button for reseting grid.

--- js/main.js
+++ js/main.js
@@ -2,29 +2,49 @@ window.onload = () => {
     console.log("connected");
 
     let btnSet = document.querySelector("#set");
-    
-    makeGrid();
-    changeColor();
+    let btnReset = document.querySelector("#reset");
+
+    makeGrid(16);
+
+    btnSet.addEventListener("click", () => {
+        let inputNumberValue = document.querySelector("#inputNumber").value;
+        
+        if(inputNumberValue != 0 && inputNumberValue >= 16 && inputNumberValue <= 100) {
+            makeGrid(inputNumberValue);
+        }
+    });
+
+    btnReset.addEventListener("click", () => {
+        makeGrid(16);
+    });
+
 }
 
 let container = document.querySelector("#container");
+let blocks = document.querySelectorAll(".gridBlock");
 
-function makeGrid() {
+function makeGrid(number) {
     let html = "";
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < number; i++) {
         html += `<div class="gridRow">`;
-        for(let j = 0; j < 16; j++) {
+        for(let j = 0; j < number; j++) {
             html += `<div class="gridBlock"></div>`;
         }
         html += `</div>`;
     }
     container.innerHTML = html;
+    changeColor();
+}
+
+function makeBlock() {
+    let html = `<div class="gridBlock"></div>`;
+    return html;
 }
 
 function changeColor() {
     let blocks = document.querySelectorAll(".gridBlock");
     blocks.forEach(block => {
-        block.addEventListener("click", () => {
+        block.addEventListener("mouseover", () => {
             block.setAttribute("style", "background-color: red;");
         });
     });
